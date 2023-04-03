a13e9172eeaa0bd27f34f10c92c4bbabef05281b
patrick11514 <ja@patrick115.eu>
2023-04-03 08:49:39 +0200

    Moved functions to separated file

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+    import { getRealSchedule, getCurrentIndex, start } from '$lib/fcs'
     import schedule from '$lib/schedule'
     import { onMount } from 'svelte'
 
@@ -11,82 +12,18 @@
         toDate: Date
     }> = []
 
-    let start = new Date()
-    /**
-     * CHANGE THIS TO THE TIME YOU WANT THE SCHEDULE TO START
-     */
-    start.setHours(15) // 15
-    start.setMinutes(0) // :00
-    start.setSeconds(0) // :00
-    start.setMilliseconds(0) // :000
-    start.setDate(2) // 2nd
-    start.setMonth(4 - 1) // April
-    /**
-     * CHANGE THIS TO THE TIME YOU WANT THE SCHEDULE TO START
-     */
-
-    realSchedule = schedule.map((sched, id) => {
-        let oneTime = sched.from.split(':')
-        let one = new Date()
-        one.setDate(start.getDate())
-        one.setMonth(start.getMonth())
-        one.setHours(parseInt(oneTime[0]))
-        one.setMinutes(parseInt(oneTime[1]))
-        one.setSeconds(0)
-        one.setMilliseconds(0)
-        if (one.getTime() < start.getTime()) {
-            one.setDate(one.getDate() + 1)
-        }
-
-        let twoTime = sched.to.split(':')
-        let two = new Date()
-        two.setDate(start.getDate())
-        two.setMonth(start.getMonth())
-        two.setHours(parseInt(twoTime[0]))
-        two.setMinutes(parseInt(twoTime[1]))
-        two.setSeconds(0)
-        two.setMilliseconds(0)
-        if (two.getTime() < start.getTime()) {
-            two.setDate(two.getDate() + 1)
-        }
-
-        if (two.getTime() == start.getTime()) {
-            if (id == schedule.length - 1) {
-                two.setDate(two.getDate() + 1)
-            }
-        }
-        return {
-            name: sched.name,
-            from: sched.from,
-            to: sched.to,
-            people: sched?.people,
-            fromDate: one,
-            toDate: two
-        }
-    })
+    realSchedule = getRealSchedule(schedule, start)
 
     let currentPixels = 0
     let currentIndex = 0
-
-    const getCurrentIndex = () => {
-        let now = new Date()
-        let index = -1
-        for (let i = 0; i < realSchedule.length; i++) {
-            if (now.getTime() > realSchedule[i].fromDate.getTime() && now.getTime() < realSchedule[i].toDate.getTime()) {
-                index = i
-                break
-            }
-        }
-        return index
-    }
 
     //timer to change offset of the schedule
     const timer = () => {
         let height = document.querySelector('#schedule')?.clientHeight
         if (height) {
             currentPixels = (new Date().getTime() - start.getTime()) / 17500
-            let index = getCurrentIndex()
-            console.log(index)
+            let index = getCurrentIndex(realSchedule)
+
             if (index > -1) {
                 currentIndex = index
             }
