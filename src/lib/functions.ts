e0d26e7bb47e3b7155b19305764e509b8b2aa3aa
Patrik Mintěl <ja@patrick115.eu>
2024-04-29 00:07:42 +0200

    Don't show seconds in table

--- src/lib/functions.ts
+++ src/lib/functions.ts
@@ -24,7 +24,6 @@ export const formatDate = (ms: number) => {
 
     const hours = date.getHours() > 9 ? date.getHours() : '0' + date.getHours();
     const minutes = date.getMinutes() > 9 ? date.getMinutes() : '0' + date.getMinutes();
-    const seconds = date.getSeconds() > 9 ? date.getSeconds() : '0' + date.getSeconds();
 
-    return `${hours}:${minutes}:${seconds}`;
+    return `${hours}:${minutes}`;
 };
